55a4ab9c885fee1cb66003dbff8fa34fd0c4fdd9
BT_1N3_27@BKTECH.LOCAL <BT_1N3_27@BKTECH.LOCAL>
2018-12-20 14:35:29 -0500

    Initial commit

---
+++ src/TokenRunner.java
@@ -0,0 +1,12 @@
+public class TokenRunner {
+    public static void main(String[] args){
+        TokenPass game1 = new TokenPass(6);
+        while(game1.gameover()<0){
+            game1.printBoard();
+            game1.distributeCurrentPlayerTokens();
+            game1.nextPlayer();
+        }
+        game1.printBoard();
+        System.out.println("Game Winner: Player"+game1.gameOver());
+    }
+}
